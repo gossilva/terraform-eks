afd222579587038263c13ee4894d0297f6601837
unknown <guilherme.derf@gmail.com>
2025-10-04 21:03:06 -0300

    feat(network): define input variables for CIDR, project name and tags Add core variables for network CIDR allocation, naming convention, and custom tag mapping for AWS resources.

---
+++ modules/network/variables.tf
@@ -0,0 +1,14 @@
+variable "cidr_block" {
+  type        = string
+  description = "Network CIDR block to be used for the VPC"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Default naming convention for EKS cluster resources"
+}
+
+variable "tags" {
+  type = map
+  description = "Tags to be added to AWS resources"
+}
