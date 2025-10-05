625c084160e4677cf92a93c3f40962533ab0ba86
unknown <guilherme.derf@gmail.com>
2025-10-05 19:10:08 -0300

    feat(eks): initial EKS cluster configuration

---
+++ modules/cluster/cluster.tf
@@ -0,0 +1,30 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name = "${var.project_name}-cluster"
+
+  access_config {
+    authentication_mode = "API"
+  }
+
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.34"
+
+  vpc_config {
+    subnet_ids = [
+      var.public_subnet_1a,
+      var.public_subnet_1b
+    ]
+    endpoint_private_access = true
+    endpoint_public_access  = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_role_attach,
+  ]
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-cluster"
+    }
+  )
+}
