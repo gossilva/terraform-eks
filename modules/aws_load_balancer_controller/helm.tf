ff1bfa20f595d19a840f938e145b4c5bc35b383a
unknown <guilherme.derf@gmail.com>
2025-10-08 22:44:37 -0300

    feat(helm): eks helm controller creation and sets definition

---
+++ modules/aws_load_balancer_controller/helm.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "eks_helm_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.14.0"
+  namespace  = "kube-system"
+
+  set = [
+
+    { name = "clusterName", value = var.clusterName },
+    { name = "serviceAccount.create", value = "false" },
+    { name = "serviceAccount.name", value = "aws-load-balancer-controller" }
+
+  ]
+}
