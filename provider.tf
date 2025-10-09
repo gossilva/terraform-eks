5d189b7f78d5e6659cddaa0273e56ccf5ec23dcc
unknown <guilherme.derf@gmail.com>
2025-10-08 22:57:58 -0300

    feat(terraform): adding kubernetes and helm provider

--- provider.tf
+++ provider.tf
@@ -4,6 +4,17 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.15.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.38.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
+
   }
 
   /*
@@ -21,3 +32,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "kubernetes" {
+  host                   = module.eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks_cluster.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+      command     = "aws"
+    }
+  }
+}
