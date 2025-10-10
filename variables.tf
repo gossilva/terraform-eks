78a7ce8b8d4d77cc851517f8e80872401d94b3e6
unknown <guilherme.derf@gmail.com>
2025-10-09 21:16:48 -0300

    refactor(terraform) making variables modular

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "project_name" {
   type        = string
   description = "Default naming convention for EKS cluster resources"
 }
+
+variable "region" {
+  type        = string
+  description = "AWS region to create the resources"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "A map of tags to add to all AWS resources"
+}
